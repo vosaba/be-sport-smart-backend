75c059e974dea09e9edde7d63c5861a859259a0d
Leonid Abasov <leonid.abasov@gmail.com>
2024-04-13 15:43:48 -0500

    feat: initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+using Azure.Identity;
+using Microsoft.Azure.Cosmos;
+using SimpleTodo.Api;
+
+var credential = new DefaultAzureCredential();
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ListsRepository>();
+builder.Services.AddSingleton(_ => new CosmosClient(builder.Configuration["AZURE_COSMOS_ENDPOINT"], credential, new CosmosClientOptions()
+{
+    SerializerOptions = new CosmosSerializationOptions
+    {
+        PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+    }
+}));
+builder.Services.AddCors();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+app.UseCors(policy =>
+{
+    policy.AllowAnyOrigin();
+    policy.AllowAnyHeader();
+    policy.AllowAnyMethod();
+});
+
+// Swagger UI
+//app.UseSwaggerUI(options => {
+//    options.SwaggerEndpoint("./openapi.yaml", "v1");
+//    options.RoutePrefix = "";
+//});
+app.UseSwagger(c =>
+{
+    c.RouteTemplate = "api/swagger/{documentName}/swagger.json";
+});
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/api/swagger/v1/swagger.json", "API");
+    //c.OAuthClientId(SwaggerConfig.ClientId);
+});
+
+app.UseStaticFiles(new StaticFileOptions{
+    // Serve openapi.yaml file
+    ServeUnknownFileTypes = true,
+});
+
+app.MapGroup("/lists")
+    .MapTodoApi()
+    .WithOpenApi();
+app.Run();
